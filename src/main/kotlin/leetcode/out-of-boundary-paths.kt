1d17e83c89ceed280a467b86ff47fc7012147f4b
gptjddldi <hystom38@gmail.com>
2024-01-27 00:04:57 +0900

    ps leetcode

--- src/main/kotlin/leetcode/out-of-boundary-paths.kt
+++ src/main/kotlin/leetcode/out-of-boundary-paths.kt
@@ -1,21 +1,23 @@
 // bfs
 // edge 좌표에서 cnt 를 넣어줌
-class Solution {
-    val mp = HashMap<Triple<Int,Int,Int>, Int>()
-    val mod = 1000000007
-    val dx = arrayOf(0,1,0,-1)
-    val dy = arrayOf(1,0,-1,0)
-    fun findPaths(m: Int, n: Int, maxMove: Int, startRow: Int, startColumn: Int): Int {
-        val key = Triple(startRow, startColumn, maxMove)
-        if(mp[key] != null) return mp.getOrDefault(key, 0)
-        if(maxMove == 0) return 0
-        var ret = 0
-        for(d in 0 .. 3) {
-            var nr = curR + dx[d]
-            var nc = curC + dy[d]
-            if(nr < 0 || nc < 0 || nr >= m || nc >= n) mp[curR][curC]++
-            else nq.add(nr to nc)
+val mp = HashMap<Triple<Int,Int,Int>, Int>()
+val mod = 1000000007
+val dx = arrayOf(0,1,0,-1)
+val dy = arrayOf(1,0,-1,0)
+fun findPaths(m: Int, n: Int, maxMove: Int, startRow: Int, startColumn: Int): Int {
+    val key = Triple(startRow, startColumn, maxMove)
+    if(mp[key] != null) return mp.getOrDefault(key, 0)
+    if(maxMove == 0) return 0
+    var ret = 0
+    for(d in 0 .. 3) {
+        var nr = startRow + dx[d]
+        var nc = startColumn + dy[d]
+        if(nr < 0 || nc < 0 || nr >= m || nc >= n) ret ++
+        else {
+            ret += findPaths(m, n, maxMove - 1, nr, nc)
+            ret %= mod
         }
-        return ret
     }
+    mp[key] = ret
+    return ret
 }
