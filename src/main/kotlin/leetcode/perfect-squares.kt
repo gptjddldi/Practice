42772447cc7e37da241d5be45350991fef3bf36b
gptjddldi <75565774+gptjddldi@users.noreply.github.com>
2024-02-08 10:19:59 +0900

    ps leetcode

---
+++ src/main/kotlin/leetcode/perfect-squares.kt
@@ -0,0 +1,16 @@
+package leetcode
+// dp[i] = min(dp[i-1], dp[i-4], dp[i-9] ... (최대 100개 )) + 1
+
+fun numSquares(n: Int): Int {
+    val dp = IntArray(n+1) { 10002 }
+    dp[0] = 0
+    for(i in 1..n) {
+        for(j in 1..i){
+            val sq = j.toDouble().pow(2)
+            if(sq <= i)
+                dp[i] = min(dp[i], dp[i-sq.toInt()]+1)
+            else break
+        }
+    }
+    return dp[n]
+}
